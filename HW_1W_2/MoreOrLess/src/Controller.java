2bb0bc3a9087a0ebfafcd977dd25a69a51a863c1
andrushka947 <likeintel9@gmail.com>
2017-07-21 15:49:02 +0300

    test

--- HW_1W_2/MoreOrLess/src/Controller.java
+++ HW_1W_2/MoreOrLess/src/Controller.java
@@ -29,7 +29,7 @@ public class Controller {
 
     private int inputNumberFromConsole(Scanner sc) {
          int input = -1;
-
+		 System.out.println("git test");
          while (input == -1) {
              try {
                  input = Integer.parseInt(sc.nextLine());
